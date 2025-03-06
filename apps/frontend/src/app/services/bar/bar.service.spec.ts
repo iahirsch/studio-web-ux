c1148a16ba0050678c9d8ee4077c0df94b3f76df
W00dPeckker <marin.hirschi@stud.hslu.ch>
2025-03-06 11:37:09 +0100

    Update bar.service.spec.ts

--- apps/frontend/src/app/services/bar/bar.service.spec.ts
+++ apps/frontend/src/app/services/bar/bar.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BarService } from './bar.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('BarService', () => {
   let service: BarService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(BarService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
