56a4245d3e6bbf0bdadf476bccc8b8360b4d316f
W00dPeckker <marin.hirschi@stud.hslu.ch>
2025-03-27 14:34:08 +0100

    fix: env.prod.ts auth0Domain

--- apps/frontend/src/env/env.prod.ts
+++ apps/frontend/src/env/env.prod.ts
@@ -1,4 +1,6 @@
 export const env = {
   production: true,
-  api: 'https://studio-web-ux-0e3341d14bde.herokuapp.com/api'
+  api: 'https://studio-web-ux-0e3341d14bde.herokuapp.com/api',
+  auth0Domain: 'https://dev-mn5tlewhdz5uqpzv.eu.auth0.com/',
+  auth0ClientId: 'USWXQqNJ8rHdNVGeBs3KdbQ6Yeu5hicj',
 };
