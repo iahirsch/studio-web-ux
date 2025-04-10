d30261cef80090d3f8d06d959dd9aa3f9dc544f2
BurkiWorld <marcoburkhardt06@gmail.com>
2025-04-10 10:56:03 +0200

    feat: fehlende child Componenten in parent Component eingefügt

--- apps/frontend/src/app/components/plate-number/plate-number.component.ts
+++ apps/frontend/src/app/components/plate-number/plate-number.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputTextComponent } from '../input-text/input-text.component';
 
 @Component({
   selector: 'app-plate-number',
-  imports: [CommonModule],
+  imports: [CommonModule, InputTextComponent],
   templateUrl: './plate-number.component.html',
-  styleUrl: './plate-number.component.css',
+  styleUrl: './plate-number.component.css'
 })
-export class PlateNumberComponent {}
+export class PlateNumberComponent {
+}
