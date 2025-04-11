ddf80934e9c511de9dc8319c3fd1cbf8d9386b57
BurkiWorld <145754185+BurkiWorld@users.noreply.github.com>
2025-04-11 15:35:26 +0200

    SCRUM-202-location-selector (#24)
    * feat: pills
    
    * style: location-select
    
    * style: accordion
    
    * style: location-selector fix
    
    * fix: lint
    
    kein div als click sondern button verwenden
    
    * fix: style höhe und dot

--- apps/frontend/src/app/components/car-details/car-details.component.ts
+++ apps/frontend/src/app/components/car-details/car-details.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PillsComponent } from '../pills/pills.component';
+import { PillItem, PillsComponent } from '../pills/pills.component';
 
 @Component({
   selector: 'app-car-details',
+  standalone: true,
   imports: [CommonModule, PillsComponent],
   templateUrl: './car-details.component.html',
   styleUrl: './car-details.component.css'
 })
-export class CarDetailsComponent {
+export class CarDetailsComponent implements OnInit {
+  carNumber = input<string>('');
+  carBrand = input<string>('');
+  carColor = input<string>('');
+  colorCode = input<string>('#000000'); // Standardwert Schwarz
+
+  carDetailsPills: PillItem[] = [];
+
+  ngOnInit(): void {
+    // Pills für die Anzeige von Auto-Details erstellen
+    this.carDetailsPills = [
+      { id: 'number', label: `${this.carNumber}` },
+      { id: 'brand', label: `${this.carBrand}` },
+      { id: 'color', label: this.carColor(), color: this.colorCode() }
+    ];
+  }
 }
