a28df9d55ff98d95c39b6a55eac1321872b0601a
Yannick Röösli <151149254+ynkroo@users.noreply.github.com>
2025-04-11 10:17:17 +0200

    SCRUM-186-card-members (#19)
    * style: card-members basic styling gemacht
    
    icon noch als svg drin – noch nicht sauber mit assets verlinkt
    
    * style: code cleanup
    
    * style: Icon now linked via asset folder
    
    * refactor: optimize imports and used angulars ngSrc
    
    ---------
    
    Co-authored-by: W00dPeckker <marin.hirschi@stud.hslu.ch>

--- apps/frontend/src/app/components/card-members/card-members.component.ts
+++ apps/frontend/src/app/components/card-members/card-members.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-card-members',
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './card-members.component.html',
   styleUrl: './card-members.component.css',
 })
