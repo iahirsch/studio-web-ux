d6a1161ffe183b708da265a1bfaa0ab3d935e6a8
mike-debastiani <145931102+mike-debastiani@users.noreply.github.com>
2025-04-15 18:09:11 +0200

    style: styling ist gemacht. Ausserdem wurde auch die Margin auf den Car/Train Cards entfernt (#34)

--- apps/frontend/src/app/components/popups/popup-ride-request-confirmation/popup-ride-request-confirmation.component.ts
+++ apps/frontend/src/app/components/popups/popup-ride-request-confirmation/popup-ride-request-confirmation.component.ts
@@ -4,12 +4,19 @@ import { MemberItemComponent } from '../../member-item/member-item.component';
 import { CardCarComponent } from '../../card-car/card-car.component';
 import { BtnPrimaryComponent } from '../../btn-primary/btn-primary.component';
 import { BtnSecondaryComponent } from '../../btn-secondary/btn-secondary.component';
+import { InputTextareaComponent } from '../../input-textarea/input-textarea.component';
 
 @Component({
   selector: 'app-popup-ride-request-confirmation',
-  imports: [CommonModule, MemberItemComponent, CardCarComponent, BtnPrimaryComponent, BtnSecondaryComponent],
+  imports: [
+    CommonModule,
+    MemberItemComponent,
+    CardCarComponent,
+    BtnPrimaryComponent,
+    BtnSecondaryComponent,
+    InputTextareaComponent,
+  ],
   templateUrl: './popup-ride-request-confirmation.component.html',
-  styleUrl: './popup-ride-request-confirmation.component.css'
+  styleUrl: './popup-ride-request-confirmation.component.css',
 })
-export class PopupRideRequestConfirmationComponent {
-}
+export class PopupRideRequestConfirmationComponent {}
